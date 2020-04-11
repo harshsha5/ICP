918822d73b0cad4d0c1022c16183984b59333945
harshsha5 <harshbitsp@gmail.com>
2020-04-11 01:12:59 -0400

    ICP working

--- ICP_fusion/toSkewSym.m
+++ ICP_fusion/toSkewSym.m
@@ -1,7 +1,11 @@
 function X = toSkewSym(x)
     
     %==== TODO: Output the 3-by-3 skew-symmetric matrix of the input 3-vector ====
-
+    beta = x(1);
+    gamma = x(2) ;
+    alpha = x(3);
     % Write your code here...
+    X = [0 -1*alpha gamma; alpha 0 -1*beta; -1*gamma beta 0];
+%     X = [1 -1*alpha gamma; alpha 1 -1*beta; -1*gamma beta 1];
     
 end
