f29854d00d63145a60bd5c84cb7bc5d905c3f285
harshsha5 <harshbitsp@gmail.com>
2020-04-11 14:43:24 -0400

    ICP vectorized and tested. All good now.

--- ICP_fusion/getRigidTransform.m
+++ ICP_fusion/getRigidTransform.m
@@ -20,17 +20,15 @@ function [tform valid_pair_num error] = getRigidTransform(new_pointcloud, ref_po
         assoc_pts = findLocalClosest(new_pts, ref_pts, m, n, d_th);
         
         %==== Set the sizes of matrix A[] and vertor b[] of normal equation: A'*A*x = A'*b ====
-%         A = zeros(m*n, 6);
-%         b = zeros(m*n, 1);
+        A = zeros(m*n, 6);
+        b = zeros(m*n, 1);
         
         %==== declare the number of point pairs that are used in this iteration ==== 
-%         valid_pair_num = 0;
         assoc_pts_2D = reshape(assoc_pts,[m*n, 3]);
         zero_counts = sum(~assoc_pts_2D,2);
         valid_pair_num = size(assoc_pts_2D,1) - sum(zero_counts()==3);  %Get number of valid  associated_pts
         non_zero_rows = zero_counts()~=3;
         valid_assoc_pts_2D = assoc_pts_2D(non_zero_rows , :);   %Get valid  associated_pts
-        A = zeros(size(valid_assoc_pts_2D,1), 6);
         
         %==== TODO: Assign values to A[] and b[] ====
         %==== (Notice: the format of the desired 6-vector is: xi = [beta gamma alpha t_x t_y t_z]') ====
@@ -40,29 +38,25 @@ function [tform valid_pair_num error] = getRigidTransform(new_pointcloud, ref_po
         ref_normals_2D =  reshape(ref_normals,[m*n, 3]);
         valid_ref_normals_2D = ref_normals_2D(non_zero_rows , :); 
         
-        b = sum((valid_ref_pts_2D - valid_assoc_pts_2D).*valid_ref_normals_2D,2);
+        b(non_zero_rows,1) = sum((valid_ref_pts_2D - valid_assoc_pts_2D).*valid_ref_normals_2D,2);
         
         % Write your code here...
-%         b = sum((ref_pts - assoc_pts).*ref_normals,3); %See if we need to remove the rows which came with points of no association
-%         b = reshape(b,[m*n, 1]); %First all column points are taken then the next column is appended at the bottom
         %==== TODO: Solve for the 6-vector xi[] of rigid body transformation ====
 
         % Write your code here...
-        A(:,4:6) = valid_ref_normals_2D;
-        A(:,1) = valid_ref_normals_2D(:,3).* valid_assoc_pts_2D(:,2) - valid_ref_normals_2D(:,2).* valid_assoc_pts_2D(:,3);
-        A(:,2) = valid_ref_normals_2D(:,1).* valid_assoc_pts_2D(:,3) - valid_ref_normals_2D(:,3).* valid_assoc_pts_2D(:,1);
-        A(:,3) = valid_ref_normals_2D(:,2).* valid_assoc_pts_2D(:,1) - valid_ref_normals_2D(:,1).* valid_assoc_pts_2D(:,2);
+        A(non_zero_rows,4:6) = valid_ref_normals_2D;
+        A(non_zero_rows,1) = valid_ref_normals_2D(:,2).* valid_assoc_pts_2D(:,3) - valid_ref_normals_2D(:,3).* valid_assoc_pts_2D(:,2);
+        A(non_zero_rows,2) = valid_ref_normals_2D(:,3).* valid_assoc_pts_2D(:,1) - valid_ref_normals_2D(:,1).* valid_assoc_pts_2D(:,3);
+        A(non_zero_rows,3) = valid_ref_normals_2D(:,1).* valid_assoc_pts_2D(:,2) - valid_ref_normals_2D(:,2).* valid_assoc_pts_2D(:,1);
         
         %pseudoinverse method to solve
-        A_pseudo = pinv(A);
-        xi = A_pseudo*b;
+%         A_pseudo = pinv(A);
+%         xi = A_pseudo*b;
        
         %Chol2 to solve
-%         As = sparse(A);
-%         [xi, ~] = solve_chol1(As, b);
-% %         xi = xi';
+        As = sparse(A);
+        [xi, ~] = solve_chol1(As, b);
 
-        
         %==== Coerce xi[] back into SE(3) ====
         %==== (Notice: tmp_tform[] is defined in the format of right-multiplication) ====
         R = toSkewSym(xi(1:3)) + eye(3);
