a56eb91f6f7206ff65d7a1cf1e83393c1593dd09
harshsha5 <harshbitsp@gmail.com>
2020-04-12 18:35:57 -0400

    Point based fusion completed.Vectorization left

--- ICP_fusion/avgProjMapWithInputData.m
+++ ICP_fusion/avgProjMapWithInputData.m
@@ -12,8 +12,40 @@ function updated_map = avgProjMapWithInputData(proj_map, input_data, alpha, h, w
 
     %==== TODO: Update all the terms in the projected map using the input data ====
     %==== (Hint: apply is_use[] as a mask in vectorization) ====
-
+    
+    %==================================================================================
     % Write your code here...
+    is_use = reshape(is_use,h*w,1);
+    input_points_2D = reshape(input_points,h*w,size(input_points,3));
+    input_colors_2D = reshape(input_colors,h*w,size(input_colors,3));
+    input_normals_2D = reshape(input_normals,h*w,size(input_normals,3));
+    updated_points = reshape(proj_points,h*w,size(proj_points,3));
+    updated_colors = reshape(proj_colors,h*w,size(proj_colors,3));
+    updated_normals = reshape(proj_normals,h*w,size(proj_normals,3));
+    updated_ccounts = reshape(proj_ccounts,h*w,1);
+    updated_times = reshape(proj_times,h*w,1);
+    alpha_1D = reshape(alpha,h*w,1);
+    
+    input_points_2D = input_points_2D(is_use,:);
+    input_colors_2D = input_colors_2D(is_use,:);
+    input_normals_2D = input_normals_2D(is_use,:);
+    proj_points_2D = updated_points(is_use,:);
+    proj_normals_2D = updated_normals(is_use,:);
+    proj_ccounts_1D = updated_ccounts(is_use);
+    alpha_1D = alpha_1D(is_use);
+    
+    updated_points(is_use,:) = ((proj_ccounts_1D.*proj_points_2D) + (alpha_1D.*input_points_2D)) ./ (proj_ccounts_1D+alpha_1D);    
+    updated_normals(is_use,:) = ((proj_ccounts_1D.*proj_normals_2D) + (alpha_1D.*input_normals_2D))./ (proj_ccounts_1D+alpha_1D);
+    updated_colors(is_use,:) = input_colors_2D;
+    updated_ccounts(is_use,:) = proj_ccounts_1D+alpha_1D;
+    updated_times(is_use,:) = t;
+    
+    %Reshaping back to their prior shapes
+    updated_points = reshape(updated_points,[h,w,3]);
+    updated_normals = reshape(updated_normals,[h,w,3]);
+    updated_colors = reshape(updated_colors,[h,w,3]);
+    updated_ccounts = reshape(updated_ccounts,[h,w]);
+    updated_times = reshape(updated_times,[h,w]);
     
     
     %==== Output the updated projected map in a struct ====
--- ICP_fusion/updateFusionMapWithProjMap.m
+++ ICP_fusion/updateFusionMapWithProjMap.m
@@ -2,9 +2,27 @@ function fusion_map = updateFusionMapWithProjMap(fusion_map, updated_map, h, w, 
 
     %==== TODO: Merge the updated map with the remaining part of the old fusion map ====
 
-    % Write your code here...
-
+    fusion_map_pointcloud_location = fusion_map.pointcloud.Location(~proj_flag,:);
+    fusion_map_pointcloud_color = fusion_map.pointcloud.Color(~proj_flag,:);
+    fusion_map_normals = fusion_map.normals(~proj_flag,:);
+    fusion_map_ccounts = fusion_map.ccounts(~proj_flag,:);
+    fusion_map_times = fusion_map.times(~proj_flag,:);
+      
+    %Reshaping new information to combine easily
+    updated_points_2D = reshape(updated_map.points, [h * w, 3]);
+    updated_colors_2D = reshape(updated_map.colors, [h * w, 3]);
+    updated_normals_2D = reshape(updated_map.normals, [h * w,3]);
+    updated_ccounts_2D = reshape(updated_map.ccounts, [h * w, 1]);
+    updated_times_2D = reshape(updated_map.times, [h * w, 1]);
     
+    %Concatenate new updated points and old points from the prior fusion
+    %map
+    map_points = cat(1, fusion_map_pointcloud_location, updated_points_2D);
+    map_colors = cat(1, fusion_map_pointcloud_color, updated_colors_2D);
+    map_normals = cat(1, fusion_map_normals, updated_normals_2D);
+    map_ccounts = cat(1, fusion_map_ccounts, updated_ccounts_2D);
+    map_times = cat(1, fusion_map_times, updated_times_2D);
+        
     %==== Output the final point-based fusion map in a struct ====
     map_pointcloud = pointCloud(map_points, 'Color', map_colors);
     fusion_map = struct('pointcloud', map_pointcloud, 'normals', map_normals, 'ccounts', map_ccounts, 'times', map_times);
